1f9bbd61b641105e8154a6701f6eba1a5a2ae8a5
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2025-08-31 03:12:55 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 126884695634066 then
-  loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/27f4304e547143a5b2edb1c92799fba5"))()
+  loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/b395d70d90ad43969784b301d1bbc3a4"))()
 end
