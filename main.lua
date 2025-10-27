d3e56ad647825fe1a52b98b1bc1858e6f86ed19d
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2025-10-27 07:36:51 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -93,7 +93,7 @@ elseif game.PlaceId == 126509999114328 then
 elseif game.PlaceId == 126884695634066 then
   loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/aec04dd265c9656e"))()
 elseif game.PlaceId == 127742093697776 then
-  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/cb19e4616be931c4"))()
+  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/06bf5c18afb5478e"))()
 elseif game.PlaceId == 9872472334 then
   loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/e76d1d2a7c54399f"))()
 end
